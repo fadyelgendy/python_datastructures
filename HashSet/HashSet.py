359c606316d5bba35e7649255027da74430b42f9
Fady El Gendy <fadyelgendy2017@gmail.com>
2023-05-06 15:05:17 +0300

    HashSet Update

--- HashSet/HashSet.py
+++ HashSet/HashSet.py
@@ -10,44 +10,38 @@ class HashSet(object):
             return False
     
         index = self.hash(key)
-
-        for i in self.data:
-            if i == index:
-                return True
-        
-        return False
+        return self.data[index] != None
     
-    def add(self, val):
-        if self.contains(val):
-            return
+    def add(self, key, val):
+        index = self.hash(key)
+        if self.contains(key):
+            self.data[index] = val
         
-        index = self.hash(val)
         self.data[index] = val
 
-    def remove(self, val):
-        if self.contains(val) is False:
+    def remove(self, key):
+        if self.contains(key) is False:
             return -1
         
-        index = self.hash(val=val)
+        index = self.hash(key)
         target = self.data[index]
 
-        del self.data[index]
+        self.data[index] = None
 
         return target
     
-    def get(self, indx):
-        if self.contains(indx) is False:
+    def get(self, key):
+        if self.contains(key) is False:
             return -1
         
-        index = self.hash(indx)
+        index = self.hash(key)
         return self.data[index]
 
     def hash(self, val):
         return val % 1000
     
     def print_hash_set(self):
-        for i in self.data:
-            if i is not None:
-                print(f'{i} ', end='')
+        for indx,val in enumerate(self.data):
+            if self.data[indx] is not None:
+                print(f"{indx}\t=>\t{self.data[indx]}")
         
-        print()
--- HashSet/main.py
+++ HashSet/main.py
@@ -3,14 +3,16 @@ from HashSet import HashSet
 if __name__ == '__main__':
     myHashSet = HashSet()
 
-    myHashSet.add(1)
-    myHashSet.add(2)
-    myHashSet.add(2)
+    myHashSet.add(1, 400)
+    myHashSet.add(2, 500)
+    myHashSet.add(2, 250)
+    myHashSet.add(600, 456)
+
+    myHashSet.print_hash_set()
 
     print(f'Contains[4]: {myHashSet.contains(4)}')
+    print(f'Contains[600]: {myHashSet.contains(600)}')
     print(f'Remove[2]: {myHashSet.remove(2)}')
     print(f'Remove[4]: {myHashSet.remove(4)}')
 
-    print(f'Get: {myHashSet.get(1)}')
-
-    myHashSet.print_hash_set()
+    print(f'Get[600]: {myHashSet.get(600)}')
